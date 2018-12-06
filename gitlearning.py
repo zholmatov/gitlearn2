a4e81eeab05443fc33fbbe9f87e5c863950e2990
zhodan <daniyar.zholmatov@gmail.com>
2018-12-06 08:44:38 +0600

    Hello bro

--- gitlearning.py
+++ gitlearning.py
@@ -1,1 +1,10 @@
-print('Hello world')
+def f():
+    a = int(input())
+    b = int(input())
+    if b == 1:
+        print(a*a)
+    elif b == 2:
+        print(a*a*a)
+    else:
+        print('Hello')
+f()
