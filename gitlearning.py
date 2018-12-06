74c7e6dec0966927a2093d62c3faf800c96bbf78
rustamovazamat <aza1mat.090400@gmail.com>
2018-12-06 09:07:42 +0600

    ozgorttum

--- gitlearning.py
+++ gitlearning.py
@@ -2,9 +2,9 @@ def f():
     a = int(input())
     b = int(input())
     if b == 1:
-        print(a*a)
+        print(a-a)
     elif b == 2:
-        print(a*a*a)
+        print(a*a-a)
     else:
         print('Hello')
 f()
