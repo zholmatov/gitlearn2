2a6e7cd1201fc70105be188cd8bd9fdaa5e7bce4
zholmatov <daniyar.zholmatov@gmail.com>
2018-12-13 09:02:44 +0600

    From me

--- gitlearning.py
+++ gitlearning.py
@@ -1,19 +1,2 @@
-def f():
-    a = int(input())
-    b = int(input())
-    if b == 1:
-        print(a*a)
-    elif b == 2:
-        print(a*a*a)
-    else:
-        print('Hello')
-
-print('HELLO')
-def Aza():
-    print('Aza looh')
-def Maks():
-    print('Maks loh')
-Aza()
-Maks()
-a = 2
-print(2*a)
+def Doni():
+    print("It's Daniyar's project")
