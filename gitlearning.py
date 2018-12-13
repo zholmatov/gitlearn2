3e6e6071949dfddb53df4c8e5fa244dd627691c3
zholmatov <daniyar.zholmatov@gmail.com>
2018-12-13 09:18:51 +0600

    From me

--- gitlearning.py
+++ gitlearning.py
@@ -1,2 +1,19 @@
-def Doni():
-    print("It's Daniyar's")
+def f():
+    a = int(input())
+    b = int(input())
+    if b == 1:
+        print(a*a)
+    elif b == 2:
+        print(a*a*a)
+    else:
+        print('Hello')
+f()
+
+
+print('HELLO')
+def Aza():
+    print('Aza looh')
+def Maks():
+    print('Maks loh')
+Aza()
+Maks()
